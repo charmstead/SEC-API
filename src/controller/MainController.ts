0c81a586d5a184cc5b06ee99cf2f83a8212dd219
tomide <tomideso@gmail.com>
2020-04-27 17:11:04 +0100

    added get route with params query

--- src/controller/MainController.ts
+++ src/controller/MainController.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
-import axios,{AxiosPromise} from 'axios';
 import { fetchFilings } from "../services/Fetch";
 
-const API_KEY = `8095b689f51de4b31d951798eb7e54f2b1dffc88454b10870fb86795539f7066`;
 
 
 export default () => {
@@ -13,6 +11,14 @@ export default () => {
     router.post('/filings', async (req: Request, res: Response, next: NextFunction) => {
 
         const {ticker,filedAt} =req.body;
+       const filings= await fetchFilings({ticker,filedAt});
+        return res.send(filings);
+
+    })
+
+    router.get('/filings', async (req: Request, res: Response, next: NextFunction) => {
+
+        const {ticker,filedAt} =req.query;
        const filings= await fetchFilings({ticker,filedAt});
         return res.send(filings);
 
